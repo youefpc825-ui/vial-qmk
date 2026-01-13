45432c850a7112a9f4f1dd6dfc5dda8174fca4dd
youefpc825-ui <youefpc825@gmail.com>
2026-01-13 21:27:51 +0200

    Create rules.mk

---
+++ keyboards/handwired/mymacropad/rules.mk
@@ -0,0 +1,12 @@
+MCU = atmega32u4
+ARCH = AVR
+BOOTLOADER = caterina
+
+# تفعيل Vial
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+
+# تفعيل القطع الإضافية (Encoder + RGB)
+ENCODER_ENABLE = yes
+RGBLIGHT_ENABLE = yes
